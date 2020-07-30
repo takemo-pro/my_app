546907dab2797b2cadf1edbfd130917c24d3ab65
Ubuntu <ubuntu@ip-172-31-46-118.ap-northeast-1.compute.internal>
2020-07-30 12:52:29 +0000

    Finish home_pages edit

--- test/controllers/home_pages_controller_test.rb
+++ test/controllers/home_pages_controller_test.rb
@@ -1,14 +1,22 @@
 require 'test_helper'
 
 class HomePagesControllerTest < ActionDispatch::IntegrationTest
+  test "should get root" do
+    get root_path
+    assert_response :success
+    assert_select 'title' , 'Home / Magic room'
+  end
+  
   test "should get home" do
     get home_pages_home_url
     assert_response :success
+    assert_select 'title' , 'Home / Magic room'
   end
 
   test "should get help" do
     get home_pages_help_url
     assert_response :success
+    assert_select 'title' , 'Help / Magic room'
   end
 
 end
