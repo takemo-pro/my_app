9c16931127a2c1f6aca7e6370ccd53432a258021
Ubuntu <ubuntu@ip-172-31-46-118.ap-northeast-1.compute.internal>
2020-07-30 12:24:40 +0000

    Edit home,help pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'home_pages#home'
   get 'home_pages/home'
   get 'home_pages/help'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
